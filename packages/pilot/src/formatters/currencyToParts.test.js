00d0f80438e9e324baa680ad504dfba9834d7c8d
Matheus Peviani Vellone <matheusvellone@gmail.com>
2020-09-17 14:43:29 -0300

    formatter: update currency formatter
    The current version of the formatter doesn't know how to deal with
    negative amounts, so we must use a Math.abs function before using it.
    This commit fix the formatter to accept and correctly deal with negative
    values.

---
+++ packages/pilot/src/formatters/currencyToParts.test.js
@@ -0,0 +1,36 @@
+import currencyToParts from './currencyToParts'
+
+describe('currencyToParts', () => {
+  it('should return the correct value when input is 2000', () => {
+    expect(currencyToParts(200000)).toEqual({ symbol: 'R$', value: '2.000,00' })
+  })
+
+  it('should return the correct value when input is 20', () => {
+    expect(currencyToParts(20)).toEqual({ symbol: 'R$', value: '0,20' })
+  })
+
+  it('should return the correct value when input is 1', () => {
+    expect(currencyToParts(1)).toEqual({ symbol: 'R$', value: '0,01' })
+  })
+
+  it('should return the correct value when input is 0.05', () => {
+    expect(currencyToParts(0.05)).toEqual({ symbol: 'R$', value: '0,00' })
+  })
+
+  it('should return the correct value when input is 0', () => {
+    expect(currencyToParts(0)).toEqual({ minusSign: '', symbol: 'R$', value: '0,00' })
+  })
+
+  it('should return the correct value when input is 1000000000032', () => {
+    expect(currencyToParts(1000000000032)).toEqual({ minusSign: undefined, symbol: 'R$', value: '10.000.000.000,32' })
+  })
+
+  it('should return the correct value when input is -1000000000032', () => {
+    expect(currencyToParts(-1000000000032)).toEqual({ minusSign: '-', symbol: 'R$', value: '10.000.000.000,32' })
+  })
+
+  it('should return the null when input is empty', () => {
+    expect(currencyToParts(null)).toBe(null)
+    expect(currencyToParts(undefined)).toBe(null)
+  })
+})
